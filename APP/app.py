e8324fd8b17a07227d9c16c998fddc3e796a80cb
MMAEagle <mic4zi98@gmail.com>
2025-06-07 20:08:41 +0300

    app.py

--- APP/app.py
+++ APP/app.py
@@ -116,10 +116,11 @@ if st.session_state.page == "main":
         st.session_state.page = "conclusion"
         st.rerun()
 
-    if st.button("Εξαγωγή νικητή ➤"):
+    if st.button("🏆 ΕΞΑΓΩΓΗ ΝΙΚΗΤΗ", use_container_width=True):
         st.session_state.skip_to_results = True
         st.session_state.page = "Αποτελέσματα"
         st.experimental_rerun()
+
 
 
 # ------- ΣΥΜΠΕΡΑΣΜΑΤΑ --------
